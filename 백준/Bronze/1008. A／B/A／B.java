97b63f0d8abd924c9d524b648b7d0298211f21a0
nrg5477 <82476657+nrg5477@users.noreply.github.com>
2024-03-11 15:22:39 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17960 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		Double A = sc.nextDouble();
+		Double B = sc.nextDouble();
+		
+		System.out.println(A/B);
+		sc.close();
+	}
+
+}
