6d4630bf384cdb73e0f2676395c9ddd73b5c1454
Ironic <82476657+nrg5477@users.noreply.github.com>
2024-05-27 09:36:13 +0900

    [Bronze II] Title: 음계, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        boolean ascending = true;
+        boolean descending = true;
+    
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                descending = false;
+            } else if (arr[i] < arr[i - 1]) {
+                ascending = false;
+            }
+        }
+    
+        if (ascending) {
+            System.out.println("ascending");
+        } else if (descending) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
