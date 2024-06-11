65770889055d46cb1b48146ed667f329ae254141
Ironic <82476657+nrg5477@users.noreply.github.com>
2024-06-11 16:06:25 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+        int max = arr[0][0];
+        int x = 1;
+        int y = 1;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    x = i + 1;
+                    y = j + 1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(x+" "+y);
+    }
+}
